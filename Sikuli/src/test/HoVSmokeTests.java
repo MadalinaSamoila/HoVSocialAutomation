582cd6ba74429bde15a34fff6deefcd132616a3a
Alexei Khilchuk <khilchuk.alexei@gmail.com>
2015-08-14 14:50:09 +0300

    Created LobbyOperations class with Hourly Bonus, Shop and Fan Page methods Added code to the HoVSmokeTests class

--- Sikuli/src/test/HoVSmokeTests.java
+++ Sikuli/src/test/HoVSmokeTests.java
@@ -41,6 +41,92 @@ public class HoVSmokeTests {
 	@Test
 	public void BuyAllCoinPackages() {
 		
+		if (LobbyOperations.isBuyButtonPresentAndClick())
+		{
+			System.out.println("[testprogress] Lobby Buy Button is Present and Clicked - OK");
+			
+			if (LobbyOperations.isShopOpened())
+			{
+				
+				
+				System.out.println("[testprogress] Shop Opened - OK");
+				
+				if (LobbyOperations.isPurchaseButtonPresentAndClick())
+				{
+					System.out.println("[testprogress] Purchase Button is Present and has been clicked - OK");
+					
+					
+					
+					if (LobbyOperations.isFbConfirmPopupPresentAndClickBuyButton(false))
+					{
+						System.out.println("[testprogress] FB Popup (General) is Present and Buy button has been clicked - OK");
+						
+						
+						
+						if (LobbyOperations.isUpsalePopupPresentAndClickSpinButton())
+						{
+							System.out.println("[testprogress] Upsale Popup is Present and Spin has been clicked - OK");
+							
+							
+							
+							if (LobbyOperations.isFbConfirmPopupPresentAndClickBuyButton(true))
+							{
+								System.out.println("[testprogress] FB Popup (Upsale) is Present and Buy button has been clicked - OK");
+								
+								
+								
+								if (LobbyOperations.isUpsalePopupCompletedPresentAndClickCollectButton())
+								{
+									System.out.println("[testprogress] Upsale Completed and Collect button has been clicked - OK");
+									
+									
+									
+									if (LobbyOperations.isCongratulationPopupPresentAndClickOkayButton())
+									{
+										System.out.println("[testprogress] Congratulation Popup is Present and Okay button has been clicked - OK");
+									}
+									else
+									{
+										System.out.println("[testprogress] Congratulation Popup is Absent or Okay button is Absent - Failed");
+									}
+								}
+								else
+								{
+									System.out.println("[teststat] Can't Find Completed Upsale Popup or click Upsale Collect Button - FAILED");
+								}
+							}
+							else
+							{
+								System.out.println("[teststat] Can't Find FB Popup or click FB Buy Button (Upsale) - FAILED");
+							}
+						}
+						else
+						{
+							System.out.println("[teststat] Can't Find Upsale Popup or Upsale Spin Button - FAILED");
+						}
+						
+					}
+					else
+					{
+						System.out.println("[teststat] Can't Find FB Popup or click FB Buy Button (General) - FAILED");
+					}
+					
+					
+				}
+				else
+				{
+					System.out.println("[teststat] Can't Click Shop Purchase Button - FAILED");
+				}
+			}
+			else
+			{
+				System.out.println("[teststat] Shop Popup is Absent - FAILED");
+			}
+		}
+		else
+		{
+			System.out.println("[testprogress] Lobby Buy Button is Absent - UNABLE_TO_CONTINUE");
+		}
 	}
 	
 	@Test
@@ -50,6 +136,33 @@ public class HoVSmokeTests {
 	
 	@Test
 	public void CollectHourlyBonus() {
+		
+		if (LobbyOperations.isTimeBonusEnabled())
+		{
+			System.out.println("[testprogress] Time Bonus Enabled - OK");
+			
+			if (LobbyOperations.clickCollect())
+			{
+				System.out.println("[testprogress] Time Bonus Clicked - OK");
+				
+				if (LobbyOperations.isCollected())
+				{
+					System.out.println("[teststat] Time Bonus Collected - OK");
+				}
+				else
+				{
+					System.out.println("[teststat] Time Bonus is not Collected - FAILED");
+				}
+			}
+			else
+			{
+				System.out.println("[teststat] Time Bonus Not Clicked - FAILED");
+			}
+		}
+		else
+		{
+			System.out.println("[testprogress] Time Bonus Disabled - UNABLE_TO_CONTINUE");
+		}
 		
 	}
 	
---
+++ Sikuli/src/test/LobbyOperations.java
@@ -0,0 +1,225 @@
+package test;
+
+import org.sikuli.script.*;
+
+
+public class LobbyOperations 
+{
+	static Screen s = new Screen();
+	
+	public static boolean isTimeBonusEnabled() throws FindFailed // works if time bonus is filled on approx 50%
+	{
+		
+		try
+		{
+						
+			s.find("lobby//TB_full.png");
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+						
+	}
+	
+	public static boolean clickCollect() throws FindFailed
+	{
+	
+		try
+		{
+						
+			s.find("lobby//TB_collectEnabled.png");
+			
+			s.click("lobby//TB_collectEnabled.png");
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isCollected() throws FindFailed
+	{
+	
+		try
+		{				
+			s.find("lobby//TB_empty_005957_remaining.png");					
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isBuyButtonPresentAndClick() throws FindFailed
+	{
+		try
+		{
+			s.find("lobby//Lobby_buyButton.png");
+			
+			s.click("lobby//Lobby_buyButton.png");
+			
+			s.wait(3.0);
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isShopOpened() throws FindFailed
+	{
+		try
+		{
+			
+			s.find("lobby//Shop_general.png");		
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isPurchaseButtonPresentAndClick() throws FindFailed
+	{
+		try
+		{
+			s.find("lobby//Shop_purchaseButton.png");
+			
+			s.click("lobby//Shop_purchaseButton.png");
+			
+			s.wait(5.0);
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isFbConfirmPopupPresentAndClickBuyButton(boolean isUpsale) throws FindFailed
+	{
+		try
+		{
+			if (isUpsale)
+			{
+				s.find("lobby//Shop_fbConfirmPaymentGeneral.png");
+			}
+			else
+			{
+				s.find("lobby//Shop_fbConfirmPaymentUpsale.png");
+			}
+			s.find("lobby//Shop_fbConfirmPaymentBuyButton.png");
+			
+			s.click("lobby//Shop_fbConfirmPaymentBuyButton.png");
+			
+			s.wait(5.0);
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isUpsalePopupPresentAndClickSpinButton() throws FindFailed
+	{
+		try
+		{
+			s.find("lobby//Shop_upsaleWheel.png");
+			
+			s.find("lobby//Shop_upsaleWheelSpinButton.png");
+			
+			s.click("lobby//Shop_upsaleWheelSpinButton.png");
+			
+			s.wait(10.0);
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isUpsalePopupCompletedPresentAndClickCollectButton() throws FindFailed
+	{
+		try
+		{
+			s.find("lobby//Shop_upsaleWheelCompleted.png");
+			
+			s.find("lobby//Shop_upsaleWheelCompletedCollectButton.png");
+			
+			s.click("lobby//Shop_upsaleWheelCompletedCollectButton.png");
+			
+			s.wait(5.0);
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean isCongratulationPopupPresentAndClickOkayButton() throws FindFailed
+	{
+		try
+		{
+			s.find("lobby//Shop_congratulation.png");
+			
+			s.find("lobby//Shop_congratulationOkayButton.png");
+			
+			s.click("lobby//Shop_congratulationOkayButton.png");
+			
+			return true;
+		}
+		catch (FindFailed e)
+		{
+			return false;
+		}
+	}
+	
+	
+	public static boolean isLobbyReturned() throws FindFailed
+	 {
+		   try
+		   {
+			   s.find("lobby//Lobby_top.png");
+		    
+			   return true;
+		   }
+		   catch (FindFailed e)
+		   {
+			   return false;
+		   }
+	  }
+	  
+	  
+	  public static boolean isFanPageOpened() throws FindFailed
+	  {
+		   try
+		   {
+			    s.find("externalPages//FanPage.png");
+			    
+			    return true;
+		    
+		   }
+		   catch (FindFailed e)
+		   {
+			    return false;
+		   }
+	  }
+}
